7728eaf82bc18006440fa5db88528c9e8457ee25
pri92 <priya.garg@outlook.com>
2017-03-04 13:36:52 +0530

    Add files via upload

---
+++ list.php
@@ -0,0 +1,12 @@
+<?php
+$sttr2=array('html','css','jquery','css','python',
+	'mongodb','angularjs','ajax','ajax tutorial','asp','wordpress','google api','learn c','c',
+	'c++','data structure','sql','database','oracle','wikipedia','opensource','django','physics',
+	'mathematics','algorithm','networking','operating system','java','english','grammer','automata'
+	,'personality development', 'php','ruby on rails','django','laravel','database queries','tree data structure'
+	,'heap','priority queue','C','C++','c#','aspnet','S2','S3','S-Lang','S-PLUS','SA-C','SabreTalk','SAIL','Scratch'
+	,'Script.NET','turbo c++','visual basic','vbscript','windows','linux','cobol','D','go','matlab','scala','swift'
+	,'apex','awk','fortran','java'
+);
+
+?>
